c07406b0feedf1a2ab914af5f77fb4c5b7b80b3c
Jai Desai <contactjaidesai@gmail.com>
2024-12-09 17:19:26 -0500

    fix other changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,26 +4,27 @@ import Home from "./pages/home";
 import DisplayOwnerViewPage from "./pages/ownerViewPage";
 import DisplayEmployeeViewPage from "./pages/employeeViewPage";
 import DisplayDriverViewPage from "./pages/driverViewPage";
-import AddOwnerPage from './pages/AddOwnerPage';
-import AddEmployeePage from './pages/AddEmployeePage';
-import AddDriverPage from './pages/AddDriverPage';
-import StartFundingPage from './pages/StartFundingPage';
-import HireEmployeePage from './pages/HireEmployeePage';
-import FireEmployeePage from './pages/FireEmployeePage';
-import RemoveDriverRolePage from './pages/RemoveDriverRolePage';
-import AddWorkerRolePage from './pages/AddWorkerRolePage';
-import AddProductPage from './pages/AddProductPage';
-import PurchaseProductPage from './pages/PurchaseProductPage';
-import RemoveProductPage from './pages/RemoveProductPage';
-import AddVanPage from './pages/AddVanPage';
-import TakeoverVanPage from './pages/TakeoverVanPage';
-import LoadVanPage from './pages/LoadVanPage';
-import DisplayProductViewPage from './pages/DisplayProductView';
-import DisplayServiceViewPage from './pages/DisplayServiceViewPage';
-import './App.css';
+import AddOwnerPage from "./pages/AddOwnerPage";
+import AddEmployeePage from "./pages/AddEmployeePage";
+import AddDriverPage from "./pages/AddDriverPage";
+import StartFundingPage from "./pages/StartFundingPage";
+import HireEmployeePage from "./pages/HireEmployeePage";
+import FireEmployeePage from "./pages/FireEmployeePage";
+import RemoveDriverRolePage from "./pages/RemoveDriverRolePage";
+import AddWorkerRolePage from "./pages/AddWorkerRolePage";
+import AddProductPage from "./pages/AddProductPage";
+import PurchaseProductPage from "./pages/PurchaseProductPage";
+import RemoveProductPage from "./pages/RemoveProductPage";
+import AddVanPage from "./pages/AddVanPage";
+import TakeoverVanPage from "./pages/TakeoverVanPage";
+import LoadVanPage from "./pages/LoadVanPage";
+import DisplayProductViewPage from "./pages/DisplayProductView";
+import DisplayServiceViewPage from "./pages/DisplayServiceViewPage";
+import "./App.css";
 import DisplayLocationView from "./components/DisplayLocationView";
 import RefuelVanPage from "./pages/RefuelVanPage";
 import DriveVanPage from "./pages/DriveVanPage";
+import AddServicePage from "./pages/AddServicePage";
 
 function App() {
   return (
@@ -34,12 +35,18 @@ function App() {
         <Route path="/employees-view" element={<DisplayEmployeeViewPage />} />
         <Route path="/drivers-view" element={<DisplayDriverViewPage />} />
         <Route
-          path="/display_location_view"
+          path="/display-location-view"
           element={<DisplayLocationView />}
         />
-        <Route path="/display-product-view" element={<DisplayProductViewPage />} />
-        <Route path="/display-service-view" element={<DisplayServiceViewPage />} />
-        
+        <Route
+          path="/display-product-view"
+          element={<DisplayProductViewPage />}
+        />
+        <Route
+          path="/display-service-view"
+          element={<DisplayServiceViewPage />}
+        />
+
         <Route path="/add-owner" element={<AddOwnerPage />} />
         <Route path="/start-funding" element={<StartFundingPage />} />
 

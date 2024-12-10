77f826ce0135d01045edc353e0095c9f36f9c53c
KaushikSriram <154042167+KaushikSriram@users.noreply.github.com>
2024-12-09 19:59:12 -0500

    Update allRoutes.js

--- backend/route/allRoutes.js
+++ backend/route/allRoutes.js
@@ -232,4 +232,18 @@ router.post("/add-service", (req, res) => {
   );
 });
 
+// [13] manage_service
+router.post("/manage-service", (req, res) => {
+  const { username, id } = req.body;
+
+  db.query(`CALL manage_service(?, ?)`, [username, id], (err, result) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).json({ error: "Failed to manage service!" });
+    }
+    res.status(200).json({ message: "Service manager updated successfully!" });
+  });
+});
+
+
 module.exports = router;
